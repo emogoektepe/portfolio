4e2d551022fea8354c958acb9ba026f8314d5e6e
emogoektepe <e.goektepe@googlemail.com>
2024-01-26 12:55:44 +0100

    landing page done

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactMeComponent } from "./contact-me/contact-me.component";
 
 @Component({
-  selector: 'app-main-content',
-  standalone: true,
-  imports: [],
-  templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+    selector: 'app-main-content',
+    standalone: true,
+    templateUrl: './main-content.component.html',
+    styleUrl: './main-content.component.scss',
+    imports: [LandingPageComponent, AboutMeComponent, MySkillsComponent, ProjectsComponent, ContactMeComponent]
 })
 export class MainContentComponent {
 
