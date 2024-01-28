3f0dd3bf6df31f2cd6c6ec4cb263081bf6b16723
emogoektepe <e.goektepe@googlemail.com>
2024-01-28 16:38:06 +0100

    projects done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
+import { ContactMeComponent } from "./main-content/contact-me/contact-me.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, ContactMeComponent]
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { SingleProjectComponent } from '../../single-project/single-project.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  imports: [SingleProjectComponent, CommonModule],
 })
-export class ProjectsComponent {
-
-}
+export class ProjectsComponent {}
