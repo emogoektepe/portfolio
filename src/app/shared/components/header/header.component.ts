97268344f3ed25abbdcc81ace4c49e95f9ede804
emogoektepe <e.goektepe@googlemail.com>
2024-02-03 15:03:10 +0100

    animations and hamburger bar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,9 +9,37 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
   @Output() isMenuOpenChange = new EventEmitter<boolean>();
   @Input() open: boolean = false;
+  imageIndex = 0;
+  imageSources = [
+    './assets/images/burger1.svg',
+    './assets/images/burger2.svg',
+    './assets/images/burger3.svg',
+    './assets/images/burger4.svg',
+    './assets/images/burger5.svg',
+  ];
+  currentImage = this.imageSources[this.imageIndex];
+
+  ngOnChanges() {
+    const interval = setInterval(() => {
+      if (!this.open) {
+        if (this.imageIndex > 0) {
+          this.imageIndex--;
+          this.currentImage = this.imageSources[this.imageIndex];
+        } else {
+          clearInterval(interval);
+        }
+      } else {
+        if (this.imageIndex < this.imageSources.length - 1) {
+          this.imageIndex++;
+          this.currentImage = this.imageSources[this.imageIndex];
+        } else {
+          clearInterval(interval);
+        }
+      }
+    }, 70);
+  }
 
   toggleMenu() {
     this.isMenuOpenChange.emit(!this.open);
--- src/app/table-of-contents/table-of-contents.component.ts
+++ src/app/table-of-contents/table-of-contents.component.ts
@@ -8,9 +8,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './table-of-contents.component.scss',
 })
 export class TableOfContentsComponent {
-
   @Output() closeMenuBar = new EventEmitter<boolean>();
-  
+
   closeTOC() {
     this.closeMenuBar.emit(false);
   }
